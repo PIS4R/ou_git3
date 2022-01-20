4c8ba8b00f912156cb8a66bd02751d20702da7e4
PIS4R <240777@edu.p.lodz.pl>
2022-01-21 00:12:40 +0100

    Zmiany dodatkowe w 03

--- main.c
+++ main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 int Policz(int a, int b) {
-    return a * b;
+    return (a+b)*(a-b);
 }
 
 int main() {
     printf("Hello, World!\n");
-    printf("W drugim branchu 2*3 = %d\n", Policz(2, 3));
+    printf("W pierwszym branchu (2+3)*(2-3)= = %d\n", Policz(2, 3));
     return 0;
 }
